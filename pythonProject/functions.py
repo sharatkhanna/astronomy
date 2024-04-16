757f9c059e7847719b81a95bf03d84fc402aabc0
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2024-04-16 17:53:33 +0530

    Created a basics gui without interactions

--- pythonProject/functions.py
+++ pythonProject/functions.py
@@ -8,16 +8,12 @@ def get_todos(filepath=FILEPATH):
     return todos_local
 
 
-print(help(get_todos))
-
-
 def write_todos(todos_arg, filepath=FILEPATH):
     """Write a text file with modified to-do's list"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
 
-print(f"My name is {__name__} and I'm outside!")
 if __name__ == "__main__":
     print("hello from functions")
     print(get_todos())
