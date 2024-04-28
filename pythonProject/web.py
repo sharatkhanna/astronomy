e7a58fee4d2784a13032a97f877c5e060c20e88c
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2024-04-28 13:25:39 +0530

    created web app py added function to display and add todos

--- pythonProject/web.py
+++ pythonProject/web.py
@@ -3,11 +3,21 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My ToDo App")
 
 for todo in todos:
-    st. checkbox(todo)
+    st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add a new todo")
+st.text_input(label="Add", placeholder="Add a new todo",
+              on_change=add_todo, key="new_todo", label_visibility='hidden')
 
 print("hello")
+
+st.session_state
