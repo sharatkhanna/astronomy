c5e5e8e53590ceafec1146729814e9abeb4638a4
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2024-05-04 07:39:14 +0530

    Added desc. outside columns

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -10,8 +10,18 @@ with col1:
 with col2:
     st.title("Ardit Sulce")
     content = """
-    Hi there! Welcome to my page. I'm Sharat and I'm learning Python programming.
-    This page is currently under development. I'm creating this using Python. Hope 
+    Hi there! Welcome to my page. I'm Sharat and I
+    'm learning Python programming.
+    This page is currently under development. 
+    I'm creating this using Python. Hope 
     to see you around soon. Have a great day.
     """
     st.info(content)
+
+content2 = """
+Here are some of the apps that I've built using Python. 
+Feel free to download and try them. Contact me to know more
+or to provide feedback!
+"""
+st.write(content2)
+
