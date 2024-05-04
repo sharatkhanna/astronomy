7be580e9b443fc356f2cf1864fd05a68862a0432
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2024-05-04 08:31:40 +0530

    Added app desc., image & links

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -25,3 +26,20 @@ or to provide feedback!
 """
 st.write(content2)
 
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:11].iterrows():
+        st.header(row["title"])
+        st.write(row["description"], )
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[11:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
