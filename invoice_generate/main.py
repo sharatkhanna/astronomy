e2da1684fbeb1531dd96218c9c53d38582dd1041
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2024-05-08 08:19:46 +0530

    added inv date

--- invoice_generate/main.py
+++ invoice_generate/main.py
@@ -7,12 +7,17 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="L", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, inv_dt = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice Nr. {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice Nr. {invoice_nr}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Date. {inv_dt}")
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
