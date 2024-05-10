adffa715db40c5d20ad6077c2836605dca6730e3
sharatkhanna <36418526+sharatkhanna@users.noreply.github.com>
2024-05-10 07:34:12 +0530

    add subject, keyword, specify language

--- api-news/main.py
+++ api-news/main.py
@@ -1,17 +1,26 @@
 import requests
 from send_email import send_email
 
+keyword = "india"
 api_key = "02a5b172855946058cd430507b4b389e"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-04-08"
-       "&sortBy=publishedAt&apiKey=02a5b172855946058cd430507b4b389e")
+url = (f"https://newsapi.org/v2/top-headlines?"
+       f"q={keyword.lower()}"
+       "&country=in"
+       "&from=2024-04-10"
+       "&sortBy=publishedAt"
+       "&apiKey=02a5b172855946058cd430507b4b389e"
+       "&language=en")
 
 request = requests.get(url)
 content = request.json()
 
 body = ""
-for article in content['articles'][0:20]:
+for article in content['articles'][0:10]:
     if article['title'] is not None:
-        body = body + article['title'] + "\n" + article['description'] + "\n\n"
+        body = (f"Subject: Top News Articles - {keyword.title()}\n"
+                + body + article['title'] + "\n"
+                + article['description'] + "\n"
+                + article["url"] + "\n\n")
 
 body = body.encode("utf-8")
 print(body)
